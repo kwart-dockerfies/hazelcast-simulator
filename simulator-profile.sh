4bc35ea55091bdce00476d6abdd3447280859743
Josef Cacek <josef.cacek@gmail.com>
2021-03-30 21:41:04 +0200

    update

--- simulator-profile.sh
+++
@@ -1,9 +0,0 @@
-export MY_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
-export PS1='\u@\h:\w \$ '
-export PATH=${SIMULATOR_HOME}/bin:${PATH}
-
-alias l='ls -CF'
-alias la='ls -A'
-alias ll='ls -alF'
-alias ls='ls --color=auto'
-
